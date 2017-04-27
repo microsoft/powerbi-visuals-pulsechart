be062a5babcdb3f3f244f38ff8c6e8effed2e555
v-makgal <v-makgal@microsoft.com>
2017-04-27 16:57:20 +0300

    fixed tooltip text disappearing issue

--- src/visual.ts
+++ src/visual.ts
@@ -1784,7 +1784,9 @@ module powerbi.extensibility.visual {
         }
 
         private static parseSettings(dataView: DataView): PulseChartSettings {
-            return PulseChartSettings.parse<PulseChartSettings>(dataView);
+            let settings: PulseChartSettings = PulseChartSettings.parse<PulseChartSettings>(dataView);
+            settings.popup.fontSize = parseInt(<any>settings.popup.fontSize);
+            return settings;
         }
 
         public enumerateObjectInstances(options: EnumerateVisualObjectInstancesOptions): VisualObjectInstanceEnumeration {
