98f689948eab61ae45d7bbead912f60c4641229b
helen508light <helen508light@gmail.com>
2018-09-27 15:13:18 +0300

    code rewriting in ES2015 style

--- src/enum/enums.ts
+++ src/enum/enums.ts
@@ -1,31 +1,29 @@
-module powerbi.extensibility.visual {
-    export enum Orientation {
-        Vertical,
-        Horizontal
-    }
+export enum Orientation {
+    Vertical,
+    Horizontal
+}
 
-    export enum PointLabelPosition {
-        Above,
-        Below,
-    }
+export enum PointLabelPosition {
+    Above,
+    Below,
+}
 
-    export enum AnimatorStates {
-        Ready,
-        Play,
-        Paused,
-        Stopped,
-    }
+export enum AnimatorStates {
+    Ready,
+    Play,
+    Paused,
+    Stopped,
+}
 
-    export enum XAxisDateFormat {
-        DateOnly = 60 * 1000 * 24,
-        TimeOnly = 60 * 1000
-    }
-    export enum XAxisPosition {
-        Center,
-        Bottom
-    }
-    export enum RunnerCounterPosition {
-        TopLeft,
-        TopRight
-    }
+export enum XAxisDateFormat {
+    DateOnly = 60 * 1000 * 24,
+    TimeOnly = 60 * 1000
+}
+export enum XAxisPosition {
+    Center,
+    Bottom
+}
+export enum RunnerCounterPosition {
+    TopLeft,
+    TopRight
 }
--- src/models/models.ts
+++ src/models/models.ts
@@ -1,186 +1,191 @@
-module powerbi.extensibility.visual {
-    // d3
-    import Axis = d3.svg.Axis;
-    import Selection = d3.Selection;
+import powerbi from "powerbi-visuals-api";
 
-    // powerbi.extensibility.utils.tooltip
-    import TooltipEnabledDataPoint = utils.tooltip.TooltipEnabledDataPoint;
+import Axis = d3.svg.Axis;
+import Selection = d3.Selection;
 
-    // powerbi.extensibility.utils.interactivity
-    import SelectableDataPoint = utils.interactivity.SelectableDataPoint;
-    import IInteractiveBehavior = utils.interactivity.IInteractiveBehavior;
-    import IInteractivityService = utils.interactivity.IInteractivityService;
+import DataViewCategoricalColumn = powerbi.DataViewCategoricalColumn;
+import DataViewMetadataColumn = powerbi.DataViewMetadataColumn;
+import DataViewValueColumnGroup = powerbi.DataViewValueColumnGroup;
 
-    // powerbi.extensibility.utils.chart
-    import LabelEnabledDataPoint = utils.chart.dataLabel.LabelEnabledDataPoint;
-    import LegendData = utils.chart.legend.LegendData;
+import { TooltipEnabledDataPoint } from "powerbi-visuals-utils-tooltiputils";
 
-    // powerbi.extensibility.utils.formatting
-    import ValueFormatterOptions = utils.formatting.ValueFormatterOptions;
+import { interactivityService } from "powerbi-visuals-utils-interactivityutils";
+import SelectableDataPoint = interactivityService.SelectableDataPoint;
+import IInteractiveBehavior = interactivityService.IInteractiveBehavior;
+import IInteractivityService = interactivityService.IInteractivityService;
 
-    // TYPES
-    export type GenericScale = TimeScale | LinearScale;
+import { legendInterfaces, dataLabelInterfaces } from "powerbi-visuals-utils-chartutils";
+import LabelEnabledDataPoint = dataLabelInterfaces.LabelEnabledDataPoint;
+import LegendData = legendInterfaces.LegendData;
 
-    // INTERFACES
-    export interface Line extends d3.svg.Line<PointXY> { }
-    export interface LinearScale extends d3.scale.Linear<any, any> { }
-    export interface TimeScale extends d3.time.Scale<any, any> { }
-    export interface VisualDataLabelsSettings {
-        show: boolean;
-        showLabelPerSeries?: boolean;
-        labelOrientation?: Orientation;
-        isSeriesExpanded?: boolean;
-        displayUnits?: number;
-        showCategory?: boolean;
-        position?: any;
-        precision?: number;
-        labelColor: string;
-        categoryLabelColor?: string;
-        fontSize?: number;
-        labelStyle?: any;
-    }
+import { valueFormatter } from "powerbi-visuals-utils-formattingutils";
+import ValueFormatterOptions = valueFormatter.ValueFormatterOptions;
 
-    export interface PointDataLabelsSettings extends VisualDataLabelsSettings {
-        position: PointLabelPosition;
-    }
+import { Orientation, PointLabelPosition } from "../enum/enums";
+import { PulseChartSettings } from "../settings";
 
-    export interface ChartDataLabelsSettings extends PointDataLabelsSettings {
-        labelDensity: string;
-    }
+// TYPES
+export type GenericScale = TimeScale | LinearScale;
 
-    export interface TooltipData {
-        value: string | Date;
-        title: string;
-        description: string;
-        offsetX?: number;
-    }
+// INTERFACES
+export interface Line extends d3.svg.Line<PointXY> { }
+export interface LinearScale extends d3.scale.Linear<any, any> { }
+export interface TimeScale extends d3.time.Scale<any, any> { }
+export interface VisualDataLabelsSettings {
+    show: boolean;
+    showLabelPerSeries?: boolean;
+    labelOrientation?: Orientation;
+    isSeriesExpanded?: boolean;
+    displayUnits?: number;
+    showCategory?: boolean;
+    position?: any;
+    precision?: number;
+    labelColor: string;
+    categoryLabelColor?: string;
+    fontSize?: number;
+    labelStyle?: any;
+}
 
-    export interface AnimationPosition {
-        series: number;
-        index: number;
-    }
+export interface PointDataLabelsSettings extends VisualDataLabelsSettings {
+    position: PointLabelPosition;
+}
 
-    export interface PointXY {
-        x: number;
-        y: number;
-    }
+export interface ChartDataLabelsSettings extends PointDataLabelsSettings {
+    labelDensity: string;
+}
 
-    export interface PrimitiveDataPoint
-        extends TooltipEnabledDataPoint, SelectableDataPoint, LabelEnabledDataPoint {
+export interface TooltipData {
+    value: string | Date;
+    title: string;
+    description: string;
+    offsetX?: number;
+}
 
-        categoryValue: any;
-        value: number;
-        categoryIndex: number;
-        seriesIndex: number;
-        highlight?: boolean;
-        key?: string;
-        labelSettings: ChartDataLabelsSettings;
-        pointColor?: string;
-    }
+export interface AnimationPosition {
+    series: number;
+    index: number;
+}
 
-    export interface DataPoint extends PrimitiveDataPoint, PointXY {
-        groupIndex: number;
-        popupInfo?: TooltipData;
-        eventSize: number;
-        runnerCounterValue: any;
-        runnerCounterFormatString: any;
-    }
+export interface PointXY {
+    x: number;
+    y: number;
+}
 
-    export interface AxisSettings {
-        formatterOptions?: ValueFormatterOptions;
-        fontColor: string;
-        color: string;
-        show: boolean;
-    }
+export interface PrimitiveDataPoint
+    extends TooltipEnabledDataPoint, SelectableDataPoint, LabelEnabledDataPoint {
 
-    export interface AxesLabels {
-        x: string;
-        y: string;
-        y2?: string;
-    }
+    categoryValue: any;
+    value: number;
+    categoryIndex: number;
+    seriesIndex: number;
+    highlight?: boolean;
+    key?: string;
+    labelSettings: ChartDataLabelsSettings;
+    pointColor?: string;
+}
 
-    export interface Series {
-        name?: string;
-        displayName: string;
-        lineIndex: number;
-        labelSettings: ChartDataLabelsSettings;
-        data: DataPoint[];
-        color: string;
-        width: number;
-        xAxisProperties?: XAxisProperties;
-        widthOfGap: number;
-    }
+export interface DataPoint extends PrimitiveDataPoint, PointXY {
+    groupIndex: number;
+    popupInfo?: TooltipData;
+    eventSize: number;
+    runnerCounterValue: any;
+    runnerCounterFormatString: any;
+}
 
-    export interface ChartData {
-        settings: PulseChartSettings;
-        columns: DataRoles<DataViewCategoricalColumn>;
-        categoryMetadata: DataViewMetadataColumn;
-        hasHighlights: boolean;
+export interface AxisSettings {
+    formatterOptions?: ValueFormatterOptions;
+    fontColor: string;
+    color: string;
+    show: boolean;
+}
 
-        dots: DataPoint[];
-        series: Series[];
-        isScalar?: boolean;
-        dataLabelsSettings: PointDataLabelsSettings;
-        axesLabels: AxesLabels;
-        hasDynamicSeries?: boolean;
-        defaultSeriesColor?: string;
-        categoryData?: PrimitiveDataPoint[];
+export interface AxesLabels {
+    x: string;
+    y: string;
+    y2?: string;
+}
 
-        categories: any[];
-        legendData?: LegendData;
+export interface Series {
+    name?: string;
+    displayName: string;
+    lineIndex: number;
+    labelSettings: ChartDataLabelsSettings;
+    data: DataPoint[];
+    color: string;
+    width: number;
+    xAxisProperties?: XAxisProperties;
+    widthOfGap: number;
+}
 
-        grouped: DataViewValueColumnGroup[];
+export interface ChartData {
+    settings: PulseChartSettings;
+    columns: DataRoles<DataViewCategoricalColumn>;
+    categoryMetadata: DataViewMetadataColumn;
+    hasHighlights: boolean;
 
-        xScale?: TimeScale | LinearScale;
-        commonYScale?: LinearScale;
-        yScales?: LinearScale[];
-        yAxis?: Axis;
+    dots: DataPoint[];
+    series: Series[];
+    isScalar?: boolean;
+    dataLabelsSettings: PointDataLabelsSettings;
+    axesLabels: AxesLabels;
+    hasDynamicSeries?: boolean;
+    defaultSeriesColor?: string;
+    categoryData?: PrimitiveDataPoint[];
 
-        widthOfXAxisLabel: number;
-        widthOfTooltipValueLabel: number;
-        heightOfTooltipDescriptionTextLine: number;
-        runnerCounterHeight: number;
-    }
+    categories: any[];
+    legendData?: LegendData;
 
-    export interface XAxisProperties {
-        values: (Date | number)[];
-        scale: TimeScale;
-        axis: Axis;
-        rotate: boolean;
-    }
+    grouped: DataViewValueColumnGroup[];
 
-    export interface DataRoles<T> {
-        Timestamp?: T;
-        Category?: T;
-        Value?: T;
-        EventTitle?: T;
-        EventDescription?: T;
-        EventSize?: T;
-        RunnerCounter?: T;
-    }
+    xScale?: TimeScale | LinearScale;
+    commonYScale?: LinearScale;
+    yScales?: LinearScale[];
+    yAxis?: Axis;
 
-    export interface ElementDimensions {
-        x: number;
-        y: number;
-        width: number;
-        height: number;
-    }
+    widthOfXAxisLabel: number;
+    widthOfTooltipValueLabel: number;
+    heightOfTooltipDescriptionTextLine: number;
+    runnerCounterHeight: number;
+}
 
-    export interface BehaviorOptions {
-        selection: Selection<any>;
-        clearCatcher: Selection<any>;
-        interactivityService: IInteractivityService;
-        hasHighlights: boolean;
-        onSelectCallback(): void;
-    }
+export interface XAxisProperties {
+    values: (Date | number)[];
+    scale: TimeScale;
+    axis: Axis;
+    rotate: boolean;
+}
 
-    export interface IPulseChartInteractiveBehavior extends IInteractiveBehavior {
-        setSelection(d: DataPoint): void;
-    }
+export interface DataRoles<T> {
+    Timestamp?: T;
+    Category?: T;
+    Value?: T;
+    EventTitle?: T;
+    EventDescription?: T;
+    EventSize?: T;
+    RunnerCounter?: T;
+}
 
-    export interface TooltipSettings {
-        dataPointColor: string;
-        marginTop: number;
-        timeHeight: number;
-    }
+export interface ElementDimensions {
+    x: number;
+    y: number;
+    width: number;
+    height: number;
+}
+
+export interface BehaviorOptions {
+    selection: Selection<any>;
+    clearCatcher: Selection<any>;
+    interactivityService: IInteractivityService;
+    hasHighlights: boolean;
+    onSelectCallback(): void;
+}
+
+export interface IPulseChartInteractiveBehavior extends IInteractiveBehavior {
+    setSelection(d: DataPoint): void;
+}
+
+export interface TooltipSettings {
+    dataPointColor: string;
+    marginTop: number;
+    timeHeight: number;
 }
